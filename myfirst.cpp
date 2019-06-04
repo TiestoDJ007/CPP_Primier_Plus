992682297491bd35bc14c82adc59636a04014409
TiestoDJ007 <jackxiao007@outlook.com>
2019-06-04 16:27:45 +0800

    initial

--- myfirst.cpp
+++ myfirst.cpp
@@ -3,11 +3,11 @@
 //
 
 #include <iostream>
-int main()
-{
+
+int main() {
     using namespace std;
     cout << "Come up and c++ me some time.";
     cout << endl;
-    cout <<"You won't regret it!"<< endl;
+    cout << "You won't regret it!" << endl;
     return 0;
 }
