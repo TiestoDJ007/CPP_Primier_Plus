c4fe2b2a09c07b2e1be3176b700176702cf1e16b
TiestoDJ007 <jackxiao007@outlook.com>
2019-06-04 15:51:58 +0800

    initial

---
+++ myfirst.cpp
@@ -0,0 +1,13 @@
+//
+// Created by a on 2019/6/4.
+//
+
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Come up and c++ me some time.";
+    cout << endl;
+    cout <<"You won't regret it!"<< endl;
+    return 0;
+}
