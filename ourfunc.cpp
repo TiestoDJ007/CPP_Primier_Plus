9341ff4b0ac8d0bb52c44753fbaf2b3164aa8f51
TiestoDJ007 <jackxiao007@outlook.com>
2019-06-10 21:11:56 +0800

    cpp primer plus

---
+++ ourfunc.cpp
@@ -0,0 +1,22 @@
+//
+// Created by a on 2019/6/10.
+//
+#include <iostream>
+
+void simon(int);
+
+int main() {
+    using namespace std;
+    simon(3);
+    cout << "Pick an integer: ";
+    int count;
+    cin >> count;
+    simon(count);
+    cout << "Done!" << endl;
+    return 0;
+}
+
+void simon(int n) {
+    using namespace std;
+    cout << "Simon says touch your toes " << n << " times." << endl;
+}
