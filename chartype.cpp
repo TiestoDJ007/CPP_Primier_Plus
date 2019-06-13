d301e5f00dba33aa507992d95543e2d95376c236
TiestoDJ007 <jackxiao007@outlook.com>
2019-06-13 11:42:52 +0800

    cpp primer plus

---
+++ chartype.cpp
@@ -0,0 +1,15 @@
+//
+// Created by a on 2019/6/12.
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    char ch;
+    cout << "Enter a character: " << endl;
+    cin >> ch;
+    cout << "Hola! ";
+    cout << "Thank you for the " << ch << " character." << endl;
+    return 0;
+}
