fd1b660d613d1ff2f2d370a4d03e961c90ae3f87
TiestoDJ007 <jackxiao007@outlook.com>
2019-06-22 10:31:00 +0800

    cpp primer plus

---
+++ fltadd.cpp
@@ -0,0 +1,13 @@
+//
+// Created by a on 2019/6/13.
+//
+#include <iostream>
+
+int main() {
+    using namespace std;
+    float a = 2.34E+22f;
+    float b = a + 1.0f;
+    cout << "a = " << a << endl;
+    cout << "b - a = " << b - a << endl;
+    return 0;
+}
