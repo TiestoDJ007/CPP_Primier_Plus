c3ed27f53d8df3c67079ea7889027863f114e700
TiestoDJ007 <jackxiao007@outlook.com>
2019-07-23 20:49:31 +0800

    cpp primer plus

---
+++ forloop.cpp
@@ -0,0 +1,14 @@
+//
+// Created by jackx on 2019/7/23.
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    int i;
+    for (i = 0; i < 5; i++)
+        cout << "C++ knows loops.\n";
+    cout << "C++ knows when to stop.\n";
+    return 0;
+}
