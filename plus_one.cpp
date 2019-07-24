eba5d59a99289d658e04dbee50ec9627751e7967
TiestoDJ007 <jackxiao007@outlook.com>
2019-07-24 16:33:50 +0800

    cpp primer plus

---
+++ plus_one.cpp
@@ -0,0 +1,14 @@
+//
+// Created by a on 2019/7/24.
+//
+#include <iostream>
+
+int main() {
+    using std::cout;
+    int a = 20;
+    int b = 20;
+    cout << "a = " << a << ": b = " << b << "\n";
+    cout << "a++ = " << a++ << ": ++b = " << ++b << "\n";
+    cout << "a = " << a << ":b = " << b << "\n";
+    return 0;
+}
